49950839dddb7f9af8fc1bfcd85bd0a56cd84226
Øyvind Gujord <oyvind_gujord@hotmail.com>
2019-04-10 16:20:20 +0200

    Updates

---
+++ precache-manifest.daa62dbdf8c803da30a050db1d0a2ca4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a224a0218f348b4d3e5ad69e206941f",
+    "url": "/portfolio/static/media/fix.4a224a02.png"
+  },
+  {
+    "revision": "fe2ce9ec81700c0d96cb",
+    "url": "/portfolio/static/css/main.8c2719ae.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "0daab492f5ce502a60dc",
+    "url": "/portfolio/static/js/2.91f9a9c1.chunk.js"
+  },
+  {
+    "revision": "4f1e17ba6046a988d7d1377f3c009064",
+    "url": "/portfolio/static/media/gujord.4f1e17ba.png"
+  },
+  {
+    "revision": "fe2ce9ec81700c0d96cb",
+    "url": "/portfolio/static/js/main.eb11a120.chunk.js"
+  },
+  {
+    "revision": "ab900cb8ffd28cd6c963db2271bd6fb5",
+    "url": "/portfolio/static/media/got.ab900cb8.png"
+  },
+  {
+    "revision": "87848e42ff3830a8f25239e8b21b6b30",
+    "url": "/portfolio/static/media/island-escape.87848e42.png"
+  },
+  {
+    "revision": "1ecd1551cbc8c9de6216f80765578c69",
+    "url": "/portfolio/static/media/letha.1ecd1551.png"
+  },
+  {
+    "revision": "6c9b31c955931c2d66b79a037a1573e7",
+    "url": "/portfolio/static/media/spacex.6c9b31c9.png"
+  },
+  {
+    "revision": "0197eb61ac983f09a678409891caf0c0",
+    "url": "/portfolio/index.html"
+  }
+];
