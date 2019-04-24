a84a2212c2b3647196c388fb710725babec70ff1
Øyvind Gujord <oyvind_gujord@hotmail.com>
2019-04-24 12:56:37 +0200

    added icons and margin to navbar

--- src/Components/Navigation/NavigationComponent.jsx
+++ src/Components/Navigation/NavigationComponent.jsx
@@ -43,19 +43,17 @@ export default class NavigationComponent extends React.Component {
 				<Collapse isOpen={this.state.isOpen} navbar>
 					<Nav className="[ ml-auto ]" navbar>
 						<NavItem>
-							<NavLink  className={home + "[ navigation__navlink ]"} href="#/">Home</NavLink>
+							<NavLink  className={home + "[ navigation__navlink ]"} href="#/"><i className="fas fa-home"></i> Home</NavLink>
 						</NavItem>
 						<NavItem>
-							<NavLink className={projects + "[ navigation__navlink ]"} href="#/projects">Projects</NavLink>
+							<NavLink className={projects + "[ navigation__navlink ]"} href="#/projects"><i className="fas fa-laptop-code"></i> Projects</NavLink>
 						</NavItem>
 						<NavItem>
-							<NavLink className={skills + "[ navigation__navlink ]"} href="#/skills">Skills</NavLink>
+							<NavLink className={skills + "[ navigation__navlink ]"} href="#/skills"><i className="fas fa-code"></i> Skills</NavLink>
 						</NavItem>
+						
 						<NavItem>
-							<NavLink className={"[ navigation__navlink ]"} href="mailto:oyvind_gujord@hotmail.com" target="_top">E-mail me</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink className={ "[ navigation__navlink ]"} rel="noopener noreferrer" target="_blank" href="https://github.com/gujoey">GitHub</NavLink>
+							<NavLink className={ "[ navigation__navlink ]"} rel="noopener noreferrer" target="_blank" href="https://github.com/gujoey"> <i className="fab fa-github"></i> GitHub</NavLink>
 						</NavItem>
 					</Nav>
 				</Collapse>
@@ -63,3 +61,10 @@ export default class NavigationComponent extends React.Component {
 		);
 	}
 }
+
+
+/*
+<NavItem>
+							<NavLink className={"[ navigation__navlink ]"} href="mailto:oyvind_gujord@hotmail.com" target="_top">E-mail me</NavLink>
+						</NavItem>
+						*/
